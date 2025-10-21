4aff62b3b3d49727103fed987cbb39e8602ecaed
JefersonSopanR <jeferson23sopan@gmail.com>
2025-10-21 09:09:36 +0200

    Sum of numbers in ArrowUp working corerctly

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const ROWS = 4;
 const COLS = 4;
 
 const startGrid = () => {
-  const emptyCell = () => Array(4).fill().map(() => ({number: '0'}))
+  const emptyCell = () => Array(4).fill().map(() => ({number: '0', lock: false}))
   const g = []
   while (g.length < 4) {
     g.push(emptyCell());
@@ -37,7 +37,7 @@ const getEmptyPositions = (grid) => {
 
 const randomNumberGenerator = (positionsAvailable) => {
   const randomNum = Math.random() < 0.9 ? 2 : 4
-  if (!positionsAvailable) {
+  if (positionsAvailable.length === 0) {
     alert("There are not positions available!!!!");
     return (-1);
   }
@@ -94,6 +94,10 @@ printGrid();
 
 
 
+const unlockElements = (tempGrid) => {
+  return tempGrid.map(row => row.map(cell => ({...cell, lock: false})))
+}
+
 
 const deepCopyGrid = (grid) => {
    return grid.map(row => row.map(cell => ({...cell})));
@@ -115,6 +119,14 @@ const updateGrid = (key) => {
           if (rowNewPos !== row) {
             tempGrid[rowNewPos][col].number = tempGrid[row][col].number;
             tempGrid[row][col].number = '0';
+          }
+          if (rowNewPos > 0) {
+            const checkIfEqual = tempGrid[rowNewPos][col].number === tempGrid[rowNewPos - 1][col].number
+            if (checkIfEqual && !tempGrid[rowNewPos - 1][col].lock) {
+              tempGrid[rowNewPos - 1][col].number = String(2 * Number(tempGrid[rowNewPos][col].number))
+              tempGrid[rowNewPos][col].number = '0';
+              tempGrid[rowNewPos - 1][col].lock = true
+            }
           }
         }
       }
@@ -173,7 +185,16 @@ const updateGrid = (key) => {
     }
     
   }
-  grid = tempGrid;
+  const emptyPositions = getEmptyPositions(tempGrid)
+  if (emptyPositions.length > 0) {
+    const { randomPosition: pos1, randomNum: num1 }= randomNumberGenerator(emptyPositions)
+    const getIndex1dTo2d = (index) => [Math.floor(index / ROWS), (index % COLS)]
+  
+    const [row1, col1] = getIndex1dTo2d(pos1);
+    tempGrid[row1][col1].number = num1;
+  }
+
+  grid = unlockElements(tempGrid);
   printGrid();
 
 }
@@ -181,6 +202,11 @@ const updateGrid = (key) => {
 document.addEventListener('keydown', (event) => {
   const key = event.key;
   if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(key)) {
+    const emptyPositions = getEmptyPositions(grid)
+    if (emptyPositions.length === 0) {
+      alert("game finish")
+      return;
+    }
     updateGrid(key);
   }
 })
